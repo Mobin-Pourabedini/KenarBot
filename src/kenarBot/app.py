4870f95b240993fb461dce58c86084f724547bc1
Mobin Pourabedini <realmobinpourabedini@gmail.com>
2025-02-14 12:56:27 +0330

    Inherit from Flask class for production compatible purposes

--- src/kenarBot/app.py
+++ src/kenarBot/app.py
@@ -8,20 +8,20 @@ import logging
 
 from .types import ChatBotMessage
 
-
 logging.basicConfig()
 logger = logging.getLogger("kenarBot")
 
-class KenarBot:
+
+class KenarBot(Flask):
     def __init__(self, divar_identification_key: str, webhook_url: str, x_api_key: str):
+        super().__init__(__name__)
         self.divar_identification_key = divar_identification_key
-        self.webhook_url = webhook_url
-        self.x_api_key = x_api_key
         self.message_handlers = []
         self.client = httpx.Client(headers={
             'Content-Type': 'application/json',
-            'X-Api-Key': self.x_api_key
+            'X-Api-Key': x_api_key
         })
+        self.route(webhook_url, methods=['POST'])(self.webhook)
 
     def send_message(self, conversation_id: str, message: str, keyboard_markup: Optional[InlineKeyboardMarkup] = None):
         url = 'https://api.divar.ir/experimental/open-platform/chatbot-conversations/{conversation_id}/messages'
@@ -35,9 +35,8 @@ class KenarBot:
             payload["buttons"] = keyboard_markup.to_dict()
 
         response = self.client.post(url, json=payload)
-
-        print(response.status_code)
-        print(response.json())
+        logger.info(f"response status code: {response.status_code}")
+        logger.info(f"response json: {response.json()}")
 
     def message_handler(self, regexp: Optional[str] = None):
         def decorator(f):
@@ -52,33 +51,30 @@ class KenarBot:
                 message_handler.process(message)
                 break
 
-    def run(self, host='0.0.0.0', port=80, debug=False):
+    def webhook(self):
+        headers = request.headers
+        logger.info(f"headers: {headers}")
+        if headers.get('Authorization') != self.divar_identification_key:
+            return Response('{"message": "unauthorized request"}', status=403)
+        data = request.get_json()
+        logger.info(f"data: {data}")
+        if data.get('type') != 'NEW_CHATBOT_MESSAGE':
+            return Response(
+                '{"message": "message sent to chatbot webhook is not of form \"NEW_CHATBOT_MESSAGE\""}',
+                status=200)
+        chatbot_message = data.get('new_chatbot_message')
+        if not chatbot_message:
+            return Response(
+                '{"message": "message sent to chatbot does not have key \"new_chatbot_message\""}',
+                status=200)
+        if chatbot_message.get('type') != 'TEXT':
+            return Response('{"message": "message type not supported for processing"}', status=200)
+        text = chatbot_message.get('text')
+        conversation_id = chatbot_message.get('conversation').get('id')
+        self._process_new_chatbot_message(ChatBotMessage(text, conversation_id))
+        return Response('{"message": "message processed"}', status=200)
+
+    def run(self, host='0.0.0.0', port=80, debug=False, **kwargs):
         if debug:
             logger.setLevel(logging.DEBUG)
-        app = Flask(__name__)
-
-        @app.route(self.webhook_url, methods=['POST'])
-        def webhook():
-            headers = request.headers
-            logger.info(f"headers: {headers}")
-            if headers.get('Authorization') != self.divar_identification_key:
-                return Response('{"message": "unauthorized request"}', status=403)
-            data = request.get_json()
-            logger.info(f"data: {data}")
-            if data.get('type') != 'NEW_CHATBOT_MESSAGE':
-                return Response(
-                    '{"message": "message sent to chatbot webhook is not of form \"NEW_CHATBOT_MESSAGE\""}',
-                    status=200)
-            chatbot_message = data.get('new_chatbot_message')
-            if not chatbot_message:
-                return Response(
-                    '{"message": "message sent to chatbot does not have key \"new_chatbot_message\""}',
-                    status=200)
-            if chatbot_message.get('type') != 'TEXT':
-                return Response('{"message": "message type not supported for processing"}', status=200)
-            text = chatbot_message.get('text')
-            conversation_id = chatbot_message.get('conversation').get('id')
-            self._process_new_chatbot_message(ChatBotMessage(text, conversation_id))
-            return Response('{"message": "message processed"}', status=200)
-
-        app.run(host=host, port=port, debug=debug)
+        super().run(host=host, port=port, debug=debug, **kwargs)
